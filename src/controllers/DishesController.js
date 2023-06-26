b45e86d58aa14aaf2dccf8373486a382b01b3225
Francisco Mateus <fmfr@outlook.com>
2023-06-26 18:23:34 -0300

    dish controller start

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -1,10 +1,31 @@
 const knex = require('../database/knex');
+const AppError = require('../utils/AppError')
 
-class CategoriesController {
-    async create (request, response){
-       const { name, category, price, description } = request.body
-    
+class DishesController {
+    async create (request, response, next){
+        const { name, category, price, description } = request.body
+        
+        const isInfoValid = [name,category,price,description].includes(null) || [name,category,price,description].includes('')
+
+        if(!isInfoValid) {
+            throw new AppError('Informações inválidas', 401)
+        }
+
+        console.log('info', name, category, price, description);
+
+        const dish = await knex('dishes').insert({
+            name,
+            price,
+            category_id: category,
+            description
+        }).returning('*')
+
+        request.dish = dish
+
+        return next()
+
+
     }
 }
 
-module.exports = CategoriesController
+module.exports = DishesController
